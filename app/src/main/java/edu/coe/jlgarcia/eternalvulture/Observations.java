ce039531eaba4bff35d854657ca15b302be593c6
Jeffrey Garcia <>
2018-02-08 17:05:36 -0600

    Removed incorrect file formats, uploaded a whole android project. I think this will do what I want.

---
+++ app/src/main/java/edu/coe/jlgarcia/eternalvulture/Observations.java
@@ -0,0 +1,17 @@
+package edu.coe.jlgarcia.eternalvulture;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by jlgarcia on 2/8/2018.
+ */
+
+public class Observations extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
