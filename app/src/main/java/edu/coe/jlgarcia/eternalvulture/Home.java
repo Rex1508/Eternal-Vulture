0724fe83264fe5695b564e15a1dcdb8b7e1c0cdc
IJTillman <ijtillman@coe.edu>
2018-02-21 20:28:10 -0600

    Connected all pages

--- app/src/main/java/edu/coe/jlgarcia/eternalvulture/Home.java
+++ app/src/main/java/edu/coe/jlgarcia/eternalvulture/Home.java
@@ -1,13 +1,29 @@
 package edu.coe.jlgarcia.eternalvulture;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class Home extends AppCompatActivity {
+public class Home extends AppCompatActivity implements View.OnClickListener{
+
+    Button collect_sample = (Button) this.findViewById(R.id.BTN_Collect_Samples);
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        collect_sample.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        Intent i = new Intent(Home.this, InitialDataInfo.class);
+
+        startActivity(i);
     }
 }
--- app/src/main/java/edu/coe/jlgarcia/eternalvulture/Observations.java
+++ app/src/main/java/edu/coe/jlgarcia/eternalvulture/Observations.java
@@ -12,6 +12,6 @@ public class Observations extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
+        setContentView(R.layout.activity_observations);
     }
 }
