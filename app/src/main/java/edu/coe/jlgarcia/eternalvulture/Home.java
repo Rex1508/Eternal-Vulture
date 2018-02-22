2815177f6ae85ceb8fb72062765bf92dd7a29b74
NOTEBOOK1\rex15_000 <jeffrey.l.garcia1@gmail.om>
2018-02-22 09:37:25 -0600

    added the rest of the page switching. Added some comments to understand the code a little better. I also removed the picture button for the showcase.

--- app/src/main/java/edu/coe/jlgarcia/eternalvulture/Home.java
+++ app/src/main/java/edu/coe/jlgarcia/eternalvulture/Home.java
@@ -1,7 +1,9 @@
 package edu.coe.jlgarcia.eternalvulture;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class Home extends AppCompatActivity {
 
@@ -9,5 +11,18 @@ public class Home extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+    }
+
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btn_Collect_Samples:
+                Intent i = new Intent(Home.this, InitialDataInfo.class);
+                startActivity(i);
+                break;
+            case R.id.btn_Review_Samples:
+                // open review page //
+
+        }
+
     }
 }
